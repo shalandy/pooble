6ffbe20592c33d9ee5cd53e20fa0e0ced1ac6b28
David Liu <davidbliu@gmail.com>
2015-09-26 20:55:53 -0700

    pages improvements

--- controllers/page-controller.js
+++ controllers/page-controller.js
@@ -1,11 +1,10 @@
 app.controller('PageController', function($scope, $http, $routeParams) {
     
-    function searchGoPost(searchTerm){
-        if(searchTerm.indexOf('#')!=-1){
-          console.log('searching for the post');
-            $http.get(tokenizedURL(ROOT_URL + '/api/get_link_post?search_term='+encodeURIComponent(searchTerm))).
+    function getPosts(tag){
+        if(tag.indexOf('#')!=-1){
+            $http.get(tokenizedURL(ROOT_URL + '/api/page_posts?tag='+encodeURIComponent(tag))).
               success(function(data, status, headers, config){
-                $scope.post = data;
+                $scope.posts = data;
                 $scope.searchPost = true;
               }).
               error(function(data, status, headers, config){
@@ -24,16 +23,14 @@ app.controller('PageController', function($scope, $http, $routeParams) {
                 $scope.searchTerm = searchTerm;
             }).
             error(function(data, status, headers, config){
-                console.log('there was an error');
-                console.log(data);
+                console.log('there was an error searching golinks');
             });
-        searchGoPost(searchTerm);
     }
     
     searchTerm = '#' + $routeParams.tag;
     $scope.title = searchTerm
     searchGoLinks(searchTerm,1);
-    searchGoPost(searchTerm);
+    getPosts(searchTerm);
     $scope.scrollToLinks = function(){
         linksPos = $('#links').position();
         window.scrollTo(linksPos.left, linksPos.top);
