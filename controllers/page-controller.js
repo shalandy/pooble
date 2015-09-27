f2a10abb81698e6ff0d260cacd1abf4eb576abff
David Liu <davidbliu@gmail.com>
2015-09-26 21:53:35 -0700

    save posts

--- controllers/page-controller.js
+++ controllers/page-controller.js
@@ -26,13 +26,75 @@ app.controller('PageController', function($scope, $http, $routeParams) {
                 console.log('there was an error searching golinks');
             });
     }
-    
+    $scope.createPost = function(){
+        content = '';
+        $scope.editMessage = 'Create a post';
+        tinyMCE.activeEditor.setContent(content);
+        $scope.editingPost = true;
+        $('#editing-buttons').show();
+    };
+    $scope.cancelEditing = function(){
+        console.log('lskfjlskdjfjlj');
+        $scope.editingPost = false;
+        $('#editing-buttons').hide();
+    };
+    $scope.editPost = function(post){
+        $scope.editMessage = 'Edit this post';
+        content = post.content;
+        $scope.postTitle = post.title;
+        tinyMCE.activeEditor.setContent(content);
+        $scope.editingPost = true;
+        $('#editing-buttons').show();
+    };
+    $scope.savePost = function(){
+        content = tinyMCE.activeEditor.getContent();
+        title = $scope.postTitle;
+        console.log(content);
+        console.log(title);
+        console.log($scope.searchTerm);
+        $.ajax({
+        url: tokenizedURL(ROOT_URL+'/api/save_page_post'),
+        type: 'POST',
+        data: {'title': title, 'tag':$scope.searchTerm, 
+              'content': content
+        },
+        success:function(data){
+          console.log(data);
+          $scope.editingPost = false;
+          window.location.reload();
+        },
+        error:function (xhr, textStatus, thrownError){
+          console.log('failed');
+        }
+      });
+    };
+    $scope.editingPost = false;
     searchTerm = '#' + $routeParams.tag;
     $scope.title = searchTerm
+    $scope.searchTerm = searchTerm;
     searchGoLinks(searchTerm,1);
     getPosts(searchTerm);
     $scope.scrollToLinks = function(){
         linksPos = $('#links').position();
         window.scrollTo(linksPos.left, linksPos.top);
-    }
+    };
+
+    tinymce.init({
+        selector: "textarea",
+        theme: "modern",
+        plugins: [
+            "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+            "searchreplace wordcount visualblocks visualchars code fullscreen",
+            "insertdatetime media nonbreaking save table contextmenu directionality",
+            "emoticons template paste textcolor colorpicker textpattern imagetools"
+        ],
+        toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+        toolbar2: "print preview media | forecolor backcolor emoticons",
+        image_advtab: true,
+        height:500,
+        templates: [
+            {title: 'Test template 1', content: 'Test 1'},
+            {title: 'Test template 2', content: 'Test 2'}
+        ]
+    });
 });
